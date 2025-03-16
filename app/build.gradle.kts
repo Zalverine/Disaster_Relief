adfc315a105e7865e663de92e8f9beee3bb46071
zalverine <ayushjha207@gmail.com>
2025-03-16 13:22:33 +0530

    json added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.gms:play-services-maps:19.1.0")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
